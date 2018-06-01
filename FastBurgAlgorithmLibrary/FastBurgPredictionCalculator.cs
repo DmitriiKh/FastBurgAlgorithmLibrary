f271bb8fe53d58fb2d618ef818941ea402fdfa98
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-06-01 19:08:11 -0300

    added function

--- FastBurgAlgorithmLibrary/FastBurgPredictionCalculator.cs
+++ FastBurgAlgorithmLibrary/FastBurgPredictionCalculator.cs
@@ -13,14 +13,14 @@ namespace FastBurgAlgorithmLibrary
         /// For details please see "vos_fastburg.pdf"
         /// </summary>
         public static void Calculate(
-            float[] inputaudio,
+            float[] inputAudio,
             float[] forwardPredictions,
             float[] backwardPredictions,
             int position,
             int coefficientsNumber,
             int historyLengthSamples)
         {
-            Initialization(inputaudio, position, coefficientsNumber, historyLengthSamples);
+            Initialization(inputAudio, position, coefficientsNumber, historyLengthSamples);
 
 
 
@@ -29,26 +29,36 @@ namespace FastBurgAlgorithmLibrary
             double ACCUM = 0.0;
 
             for (int I = 1; I <= coefficientsNumber; I++)
-                ACCUM += inputaudio[position - I] * (-1) * a[I];
+                ACCUM += inputAudio[position - I] * (-1) * a[I];
 
             forwardPredictions[position] = (float)ACCUM;
 
             ACCUM = 0.0;
             for (int I = 1; I <= coefficientsNumber; I++)
-                ACCUM += inputaudio[position - historyLengthSamples + I] *
+                ACCUM += inputAudio[position - historyLengthSamples + I] *
                     (-1) * a[I];
 
             backwardPredictions[position - historyLengthSamples] =
                 (float)ACCUM;
         }
 
-        private static void Initialization(float[] inputaudio, int position, int coefficientsNumber, int historyLengthSamples)
+        private static void Initialization(float[] inputAudio, int position, int coefficientsNumber, int historyLengthSamples)
         {
+            double[] c = FindAutocorrelation(inputAudio, position, coefficientsNumber, historyLengthSamples);
+        }
+
+        private static double[] FindAutocorrelation(float[] inputAudio, int position, int coefficientsNumber, int historyLengthSamples)
+        {
+            double[] c = new double[coefficientsNumber + 1];
+
             for (int j = 0; j <= coefficientsNumber; j++)
             {
-                for (int )
-                c[j] += 5;
+                c[j] = 0;
+                for (int index = position - historyLengthSamples; index <= historyLengthSamples - 1 - j; index++)
+                    c[j] += inputAudio[index] * inputAudio[index + j];
             }
+
+            return c;
         }
     }
 }
