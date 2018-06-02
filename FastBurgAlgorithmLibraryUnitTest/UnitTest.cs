9bbce1e7f6999e0d8c10c2ac3902a9a46703f5a0
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-06-02 09:15:22 -0300

    Added Unit Test project

---
+++ FastBurgAlgorithmLibraryUnitTest/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using Xunit;
+
+namespace FastBurgAlgorithmLibraryUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
