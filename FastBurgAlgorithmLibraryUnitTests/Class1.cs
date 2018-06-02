058d73ae4db97577a951caaabac40409dbb70909
DmitriiKh <31587652+DmitriiKh@users.noreply.github.com>
2018-06-02 13:20:40 -0300

    Removed failing test

--- FastBurgAlgorithmLibraryUnitTests/Class1.cs
+++ FastBurgAlgorithmLibraryUnitTests/Class1.cs
@@ -10,12 +10,6 @@ namespace FastBurgAlgorithmLibraryUnitTests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
         int Add(int x, int y)
         {
             return x + y;
